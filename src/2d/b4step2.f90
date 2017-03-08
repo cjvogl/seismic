56e61e0d7f3d89190e3ec8784c610f53661b03b0
David Ketcheson <dketch@gmail.com>
2017-03-07 20:14:54 -0800

    It's running!!!  But gives NaNs. :-(

--- src/2d/b4step2.f90
+++ src/2d/b4step2.f90
@@ -9,7 +9,7 @@ subroutine b4step2(mbc,mx,my,meqn,q,xlower,ylower,dx,dy,t,dt,maux,aux)
     implicit none
     integer, intent(in) :: mbc,mx,my,meqn,maux
     real(kind=8), intent(in) :: xlower,ylower,dx,dy,t,dt
-    real(kind=8), intent(inout) :: q(meqn,1-mbc:mx+mbc,1-mbc:my+mbc)
+    real(kind=8), intent(in) :: q(meqn,1-mbc:mx+mbc,1-mbc:my+mbc)
     real(kind=8), intent(inout) :: aux(maux,1-mbc:mx+mbc,1-mbc:my+mbc)
 
     integer :: i, j, k
